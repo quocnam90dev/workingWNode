9625725f4fce12fe35289327776c20ba16a65955
beastLion <quocnam90developer@gmail.com>
2021-08-21 21:01:45 +0700

    the first deploy on heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const createServer = require('./server')
+const port = process.env.PORT || 5000
 mongoose
   .connect("mongodb://localhost:27017/testDB", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     const app = createServer()
-    app.listen(5000, () => {
+    app.listen(port, () => {
       console.log('Server has started!')
     })
   })
