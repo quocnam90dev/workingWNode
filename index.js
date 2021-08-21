b6bfaf19b7dd0a4acf268fb99cb61bc4d94f52af
beastLion <quocnam90developer@gmail.com>
2021-08-21 21:22:42 +0700

    change url db

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const createServer = require('./server')
 const port = process.env.PORT || 5000
+const DB_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/testDB"
 mongoose
-  .connect("mongodb://localhost:27017/testDB", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     const app = createServer()
     app.listen(port, () => {
